fededcecc03b76b014fca00c953dfbfca6422fe4
Steven Mendoza <smendoza171@gmail.com>
2022-12-15 07:52:46 -0800

    weather data and time update on intervals

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -10,14 +10,37 @@ import { defaultWeatherData } from "./data/defaulltData";
 
 
 export default function Weather() {
-
     const apiKey = "014cab97ef25e0042a32c8f3eca831b7";
     const baseUrl = "https://api.openweathermap.org/data/2.5/";
 
     const [coordinates, setCoordinates] = useState(defaultWeatherData.coord);
     const [weatherData, setWeatherData] = useState(defaultWeatherData);
+    const [time, setTime] = useState(new Date());
 
+  function getWeatherData() {
+    let apiUrl = `${baseUrl}weather?lat=${coordinates.lat}&lon=${
+      coordinates.lon
+    }&appid=${apiKey}&units=${"imperial"}`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setWeatherData(data);
+      });
+  }
 
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setTime(new Date());
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        getWeatherData();
+      }, 10000);
+      return () => clearInterval(interval);
+    }, []);
 
     useEffect(() => {
       navigator.geolocation.getCurrentPosition(function (position) {
@@ -28,53 +51,69 @@ export default function Weather() {
     }, []); 
 
     useEffect(() => {
-      let apiUrl = `${baseUrl}weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${"imperial"}`;
-      console.log(apiUrl);
-      fetch(apiUrl)
-        .then((res) => res.json())
-        .then((data) => {
-          setWeatherData(data);
-                console.log(data);
-
-        });
+      getWeatherData();
     }, [coordinates]); 
 
     return (
       <div className="Weather">
-        <h1 className="">Weather Central</h1>
+        <div className="weather-header">
+          <a href="/">
+            <h1 className="weather-title">WeatherCentral</h1>
+          </a>
+        </div>
 
         <div className="weather-datetime">
-          <p className="weather-time">6:07 AM</p>
-          <div className="">
-            <p className="weather-date">6 Dec</p>
-            <p className="weather-day">Thursday</p>
+          <p className="weather-time">
+            {time.toLocaleTimeString("en-US", {
+              hour: "numeric",
+              minute: "numeric",
+            })}
+          </p>
+          <div className="weather-date">
+            <p className="">
+              {time.toLocaleDateString("en-US", {
+                day: "numeric",
+                month: "short",
+              })}
+            </p>
+            <p className="weather-day">
+              {time.toLocaleDateString("en-US", { weekday: "long" })}
+            </p>
           </div>
         </div>
 
-        <p className="weather-location">{weatherData.name}</p>
-        {/* <div className="flex">
-          <p className="">{coordinates.lat.toFixed(3)}</p>
-          <p className="">{coordinates.lon.toFixed(3)}</p>
-        </div> */}
-
-        <p className="weather-temp">{weatherData.main.temp}</p>
-
-        <div className="flex weather-temp-range">
-          <p className="">L: {weatherData.main.temp_min}</p>
-          <p className="">H: {weatherData.main.temp_max}</p>
-        </div>
-
-        <p className="weather-temp-feels">
-          Feels Like: {weatherData.main.feels_like}
-        </p>
-        
-        <div className="flex weather-stats">
-          <p className="">Pressure: {weatherData.main.pressure}</p>
-          <p className="">Humidity: {weatherData.main.humidity}</p>
-          <p className="">
-            <i className="fa-solid fa-wind"></i> {weatherData.wind.speed}
+        <div className="weather-main">
+          <p className="weather-location">{weatherData.name}</p>
+          <p className="weather-temp">
+            {weatherData.main.temp.toFixed(0)}&deg;
           </p>
+          <p className="weather-status">{weatherData.weather[0].main}</p>
+          <div className="flex weather-temp-range">
+            <p className="">L:{weatherData.main.temp_min.toFixed(0)}&deg;</p>
+            <p className="">H:{weatherData.main.temp_max.toFixed(0)}&deg;</p>
+          </div>
         </div>
+
+
       </div>
     );
 }
+
+
+
+/* <div className="flex">
+<p className="">{coordinates.lat.toFixed(3)}</p>
+<p className="">{coordinates.lon.toFixed(3)}</p>
+</div> */
+
+/* <p className="weather-temp-feels">
+Feels Like: {weatherData.main.feels_like.toFixed(0)}&deg;
+</p> */
+
+/* <div className="flex weather-stats">
+<p className="">Pressure: {weatherData.main.pressure}</p>
+<p className="">Humidity: {weatherData.main.humidity}</p>
+<p className="">
+  <i className="fa-solid fa-wind"></i> {weatherData.wind.speed}
+</p>
+</div> */
